539006246e5ba8f9e73ba8f1a27247c05c4988af
alexandre.bezerra <gbalexandre@hotmail.com>
2020-07-03 22:32:06 -0300

    Servlet de login

--- src/br/com/impacta/javawebservlets/lab/SistemaServlet.java
+++ src/br/com/impacta/javawebservlets/lab/SistemaServlet.java
@@ -1,11 +1,14 @@
 package br.com.impacta.javawebservlets.lab;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import br.com.impacta.javawebservlets.lab.bean.Usuario;
 
 /**
  * Servlet implementation class SistemaServlet
@@ -18,8 +21,13 @@ public class SistemaServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("<HTML><BODY><H1>Bem Vindo " + request.getParameter("login") + "!")
+
+
+		//Usuario usuario = (Usuario) request.getSession().getAttribute("usuario");
+		Usuario usuario = (Usuario) request.getAttribute("usuario");
+		
+				
+		response.getWriter().append("<HTML><BODY><H1>Bem Vindo " + usuario.getLogin() + "!")
 							.append("<BR>Esta é a pagina principal do sistema.")
 							.append("<BR><BR><a href=\"/servlet-login/login.html\">Logout</a>")
 							.append("</BODY></HTML");
--- src/br/com/impacta/javawebservlets/lab/ValidaLogin.java
+++ src/br/com/impacta/javawebservlets/lab/ValidaLogin.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.catalina.connector.Response;
 
+import br.com.impacta.javawebservlets.lab.bean.Usuario;
+
 /**
  * Servlet implementation class ValidaLogin
  */
@@ -22,7 +24,25 @@ public class ValidaLogin extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String login = request.getParameter("login");
 		String senha = request.getParameter("senha");
+		
+		//Usuario usuario = (Usuario) request.getSession().getAttribute("usuario");
+		Usuario usuario = (Usuario) request.getAttribute("usuario");
+		
+		if (usuario == null) {
+			 usuario = new Usuario();
+						usuario.setLogin(login);
+			usuario.setSenha(senha);
+			
+			//request.getSession().setAttribute("usuario", usuario);
+			request.setAttribute("usuario", usuario);
+		}
+				
+				
+
+					
 		
 		if (senha != null && senha.equals("impacta123")) {
 			
